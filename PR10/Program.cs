2167c9b75eee12e8111484895ae1ed28e583bf6a
Ronen302 <ronen302@gmail.com>
2021-09-30 13:17:13 +0300

    Добавил строку 14

--- PR10/Program.cs
+++ PR10/Program.cs
@@ -11,6 +11,7 @@ namespace PR10
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World");
+            Console.WriteLine("Hello World2");
         }
     }
 }
