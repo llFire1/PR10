be67bffe9ff051e28419e254aa93037eff5c2fcc
Rustam <ik3895700@gmail.com>
2021-09-30 13:40:40 +0300

    Добавил Console.WriteLine("Привет салённая кароба");, номер 15

--- PR10/Program.cs
+++ PR10/Program.cs
@@ -12,6 +12,7 @@ namespace PR10
         {
             Console.WriteLine("Hello World");
             Console.WriteLine("Hello World2");
+            Console.WriteLine("Привет салённая кароба");
         }
     }
 }
